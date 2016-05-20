099a85919e30ab84a85ebdba4356af1539a2cb50
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2016-05-20 20:05:11 +0200

    nothing working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require 
+//= require blockrain/src/blockrain.jquery.libs.js
+//= require blockrain/src/blockrain.jquery.src.js
+//= require blockrain/src/blockrain.jquery.themes.js
